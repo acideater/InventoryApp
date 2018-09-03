0a4a15a3a055816908fac815de710c05a67d5bb6
AnaPaula <anapauladfp@gmail.com>
2018-09-03 17:59:55 -0400

    Add EditorActivity, menu_editor and activity_editor layout.

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -1,6 +1,78 @@
 package com.example.android.inventoryapp;
 
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    /**
+     * EditText field to enter the product's name
+     */
+    private EditText mNameEditText;
+
+    /**
+     * EditText field to enter the product's price
+     */
+    private EditText mPriceEditText;
+
+    /**
+     * EditText field to enter the product's quantity
+     */
+    private EditText mQuantityEditText;
+
+    /**
+     * EditText field to enter the product's Supplier name
+     */
+    private EditText mSupplierNameText;
+
+    /**
+     * EditText field to enter the product's Supplier contact info
+     */
+    private EditText mSupplierContactText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = findViewById(R.id.edit_product_name);
+        mPriceEditText = findViewById(R.id.edit_product_price);
+        mQuantityEditText = findViewById(R.id.edit_product_quantity);
+        mSupplierNameText = findViewById(R.id.edit_supplier_name);
+        mSupplierContactText = findViewById(R.id.edit_supplier_name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
