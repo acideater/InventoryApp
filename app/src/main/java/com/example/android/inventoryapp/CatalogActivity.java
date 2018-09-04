7b2c06fecfecac445f58cbb711d8a47cb58ab461
AnaPaula <anapauladfp@gmail.com>
2018-09-03 21:32:01 -0400

    Admend commit messages

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.inventoryapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.FloatingActionButton;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
+import com.example.android.inventoryapp.data.ProductDbHelper;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -17,7 +23,7 @@ public class CatalogActivity extends AppCompatActivity {
 
 
         // Setup FAB to open EditorActivity
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +31,33 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the products database.
+     */
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        ProductDbHelper mDbHelper = new ProductDbHelper(this);
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        // Perform this raw SQL query "SELECT * FROM products"
+        // to get a Cursor that contains all rows from the products table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + ProductEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // products table in the database).
+            TextView displayView = findViewById(R.id.text_view_product);
+            displayView.setText("Number of rows in products database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 
     @Override
