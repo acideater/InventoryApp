6eb8f3e5612d5d8302e35a045508ac9fea6975c8
acideater <anapauladfp@gmail.com>
2018-09-22 14:47:11 -0400

    Implement and use ContentProvider insert() method

--- app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -7,6 +7,7 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
 
@@ -14,6 +15,10 @@ import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
  * {@link ContentProvider} for Inventory app.
  */
 public class ProductProvider extends ContentProvider {
+    /**
+     * Tag for the log messages
+     */
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
 
     /**
      * URI matcher code for the content URI for the products table
@@ -107,7 +112,31 @@ public class ProductProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return insertProduct(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a product into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertProduct(Uri uri, ContentValues values) {
+        // Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        // Insert the new product with the given values
+        long id = database.insert(ProductEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
