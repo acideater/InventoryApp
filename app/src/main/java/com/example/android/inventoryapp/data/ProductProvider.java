b3c9d1bdb8f38f4b7154865e78b0acdf8f582968
acideater <anapauladfp@gmail.com>
2018-09-22 14:21:28 -0400

    Add constants for products content URIs and UriMatcher to ContentProvider

--- app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -2,13 +2,50 @@ package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 /**
- * {@link ContentProvider} for Pets app.
+ * {@link ContentProvider} for Inventory app.
  */
 public class ProductProvider extends ContentProvider {
+
+    /**
+     * URI matcher code for the content URI for the products table
+     */
+    private static final int PRODUCTS = 100;
+    /**
+     * URI matcher code for the content URI for a single product in the products table
+     */
+    private static final int PRODUCT_ID = 101;
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        // The content URI of the form "content://com.example.android.inventoryapp/products" will
+        // map to the integer code {@link #PRODUCTS}. This URI is used to provide access to
+        // MULTIPLE rows of the products table.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS, PRODUCTS);
+        // The content URI of the form "content://com.example.android.inventoryapp/products/#" will
+        // map to the integer code {@link #PRODUCT_ID}. This URI is used to provide access to ONE
+        // single row of the products table.
+        //
+        // In this case, the "#" wildcard is used where "#" can be substituted for an integer.
+        // For example, "content://com.example.android.inventoryapp/products/3" matches, but
+        // "content://com.example.android.inventoryapp/products" (without a number at the end)
+        // doesn't match.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS + "/#", PRODUCT_ID);
+    }
+
     /**
      * Database helper object
      */
