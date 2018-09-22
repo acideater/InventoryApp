f2cac843945895984ea372aaa31454e894a35ef9
acideater <anapauladfp@gmail.com>
2018-09-22 19:10:25 -0400

    Implement ContentProvider delete() method

--- app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -160,11 +160,6 @@ public class ProductProvider extends ContentProvider {
     }
 
     @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        return 0;
-    }
-
-    @Override
     public int update(Uri uri, ContentValues contentValues, String selection,
                       String[] selectionArgs) {
         final int match = sUriMatcher.match(uri);
@@ -225,4 +220,25 @@ public class ProductProvider extends ContentProvider {
         // Returns the number of database rows affected by the update statement
         return database.update(ProductEntry.TABLE_NAME, values, selection, selectionArgs);
     }
+
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+            case PRODUCT_ID:
+                // Delete a single row given by the ID in the URI
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+    }
+
 }
