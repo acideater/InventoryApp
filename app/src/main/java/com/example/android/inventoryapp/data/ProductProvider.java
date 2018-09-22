ce4002e72832403d103acbbdccdc8ed2989a9b4a
acideater <anapauladfp@gmail.com>
2018-09-22 14:10:12 -0400

    Add ContentProvider

---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -0,0 +1,47 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * {@link ContentProvider} for Pets app.
+ */
+public class ProductProvider extends ContentProvider {
+    /**
+     * Database helper object
+     */
+    private ProductDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new ProductDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
